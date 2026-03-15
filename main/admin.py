54613f305d9a075c29e404937fb4f11210a69d42
boykovikevg <boykovikevg@gmail.com>
2026-03-15 15:16:34 +0200

    Фиксация 3

--- main/admin.py
+++ main/admin.py
@@ -1,27 +1,37 @@
 from django.contrib import admin
+from .models import Size, Category, \
+    ClothingItem, ClothingItemSize, ItemImage
 
-from .models import Size, Category, ClothingItem, ClothingItemSize
 
-
-class ClothingItemSizeInLine(admin.TabularInline):
+class ClothingItemSizeInline(admin.TabularInline):
     model = ClothingItemSize
     extra = 4
-    
+
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
+    extra = 4
+
+
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
     list_display = ('name',)
-    search_field= ('name',)
+    search_fields = ('name',)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','slug')
+    list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
-    search_field= ('name',)
-    
+    search_fields = ('name',)
+
+
 @admin.register(ClothingItem)
 class ClothingItemAdmin(admin.ModelAdmin):
-    list_display = ('name','slug','category', 'available','price','discount','created_at','update_at')
+    list_display = ('name', 'slug', 'category',
+                    'available', 'price', 'discount',
+                    'created_at', 'updated_at')
     list_filter = ('available', 'category')
     prepopulated_fields = {'slug': ('name',)}
     ordering = ('-created_at',)
-    inlines = [ClothingItemSizeInLine]
+    inlines = [ClothingItemSizeInline, ItemImageInline]
--- main/views.py
+++ main/views.py
@@ -1,53 +1,70 @@
 from django.views.generic import ListView, DetailView
-from .models import ClothingItem, Category, Size
+from .models import ClothingItem, Category, Size, \
+    ClothingItemSize
 from django.db.models import Q
+
 
 class CatalogView(ListView):
     model = ClothingItem
     template_name = 'main/product/list.html'
-    context_object_name = 'cloting_items' # У шаблоні буде доступно як cloting_items
-    
+    context_object_name = 'clothing_items'
+
+
     def get_queryset(self):
         queryset = super().get_queryset()
-        # Отримуємо списки з GET-запиту
-        category_slugs = self.request.GET.getlist('category') # Виправлено назву змінної
+        category_slugs = self.request.GET.getlist('category')
         size_names = self.request.GET.getlist('size')
         min_price = self.request.GET.get('min_price')
-        max_price = self.request.GET.get('max_price') 
-        
-        if category_slugs:
+        max_price = self.request.GET.get('max_price')
+        search_query = self.request.GET.get('q')
+
+        if category_slugs: 
             queryset = queryset.filter(category__slug__in=category_slugs)
 
         if size_names:
-            # Використовуємо distinct(), щоб уникнути дублікатів товарів, 
-            # якщо у товара є кілька розмірів, що підпадають під фільтр
             queryset = queryset.filter(
-                sizes__name__in=size_names,
-                sizes__clothingitemsize__available=True # Перевір назву Related Name у моделі
+                Q(sizes__name__in=size_names) & Q(sizes__clothingitemsize__available=True)
             ).distinct()
-            
+        
         if min_price:
-            queryset = queryset.filter(price__gte=min_price)       
-                           
+            queryset = queryset.filter(price__gte=min_price)
+
         if max_price:
-            queryset = queryset.filter(price__lte=max_price)  
-                                       
+            queryset = queryset.filter(price__lte=max_price)
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(description__icontains=search_query)
+            ).distinct()
+
         return queryset
     
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
-        context['sizes'] = Size.objects.all() # Виправлено на 'sizes' для логіки
+        context['sizes'] = Size.objects.all()
         context['selected_categories'] = self.request.GET.getlist('category')
         context['selected_sizes'] = self.request.GET.getlist('size')
         context['min_price'] = self.request.GET.get('min_price', '')
         context['max_price'] = self.request.GET.get('max_price', '')
-        return context
 
-# Виніс клас DetailView з CatalogView (раніше він був вкладеним)
+        return context
+    
+
 class ClothingItemDetailView(DetailView):
     model = ClothingItem
     template_name = 'main/product/detail.html'
     context_object_name = 'clothing_item'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        clothing_item = self.object
+        available_sizes = ClothingItemSize.objects.filter(clothing_item=clothing_item,
+                                                           available=True)
+        context['available_sizes'] = available_sizes
+        return context
